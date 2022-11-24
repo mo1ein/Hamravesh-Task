afc9091981bb9062f727229c4411285bc7e5fcdd
mo1ein <moeinn.com@gmail.com>
2022-11-24 10:38:55 +0330

    Add run url

--- config/urls.py
+++ config/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('apps/', include('apps.urls')),
     path('create/', CreateApp.as_view()),
-    # path('run/', RunList.as_view())
+    path('run/', RunList.as_view())
 ]
