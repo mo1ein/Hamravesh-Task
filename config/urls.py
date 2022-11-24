b63ad6d4472d10d693da0a6ce9ed35cd9dad9da2
mo1ein <moeinn.com@gmail.com>
2022-11-24 08:50:53 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,13 @@
 # config/urls.py
 from django.contrib import admin
 from django.urls import include, path
-from apps.views import CreateApp, RunAppList, home_view
+from apps.views import CreateApp, home_view
 from appmanager.views import RunList
 
 urlpatterns = [
     path('', home_view),
     path('admin/', admin.site.urls),
     path('apps/', include('apps.urls')),
-    path('create', CreateApp.as_view()),
-    path('run', RunList.as_view())
+    path('create/', CreateApp.as_view()),
+    # path('run/', RunList.as_view())
 ]
