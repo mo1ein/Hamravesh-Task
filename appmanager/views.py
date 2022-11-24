a6d55b9d549271ce438f22ee72b871a9f4ae35c2
mo1ein <moeinn.com@gmail.com>
2022-11-24 08:58:13 +0330

    Fix imports in appmanager/views

--- appmanager/views.py
+++ appmanager/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
-
+from .serializers import RunSerializer
 from appmanager import models
 
 
@@ -10,7 +10,7 @@ class CreateRun(generics.CreateAPIView):
     queryset = models.Run.objects.all()
     serializer_class = RunSerializer
 
+
 class RunList(generics.ListAPIView):
     queryset = models.Run.objects.all()
     serializer_class = RunSerializer
-
