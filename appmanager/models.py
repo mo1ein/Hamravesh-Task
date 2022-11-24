61d58ce7db75431c58380b954520aeea7795ef3a
mo1ein <moeinn.com@gmail.com>
2022-11-24 08:49:49 +0330

    Refactor appmanager

--- appmanager/models.py
+++ appmanager/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Run(models.Model):
     # todo: set defaults and fix datatypes
     # fix size of lengthes
-    name = models.CharField(max_length=200)
+    app_name = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     # or parameters
     envs = models.CharField(max_length=200)
@@ -13,4 +13,4 @@ class Run(models.Model):
     time = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.app_name
--- appmanager/serializers.py
+++ appmanager/serializers.py
@@ -7,10 +7,11 @@ from appmanger import models
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
-        'name',
-        'image',
-        'envs',
-        'time',
-        'status',
-    )
-    model = models.Run
+        fields = (
+            'app_name',
+            'image',
+            'envs',
+            'status',
+            'time',
+        )
+        model = models.Run
