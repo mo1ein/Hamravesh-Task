a98167a684e5c4e80c3eda941b3af52ce870440b
mo1ein <moeinn.com@gmail.com>
2022-11-28 01:51:26 +0330

    Add RegexValidator for image url

--- apps/models.py
+++ apps/models.py
@@ -1,14 +1,22 @@
 # apps/models.py
+
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class App(models.Model):
-    # todo: set defaults
-    # fix size of lengthes
     name = models.CharField(max_length=200)
-    # must be url type ...
-    # image = models.URLField(max_length=200)
-    image = models.CharField(max_length=200)
+    # validate image url format
+    image = models.CharField(
+        max_length=200,
+        validators=[
+            RegexValidator(
+                regex=r'^(?:(?=[^:\/]{1,253})(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*(?::[0-9]{1,5})?/)?((?![._-])(?:[a-z0-9._-]*)(?<![._-])(?:/(?![._-])[a-z0-9._-]*(?<![._-]))*)(?::(?![.-])[a-zA-Z0-9_.-]{1,128})?$',
+                message='image is not valid',
+                code='invalid_url'
+            )
+        ]
+    )
     # todo: must be list
     envs = models.CharField(max_length=200)
     command = models.CharField(max_length=200)
